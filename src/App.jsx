025f4c9875fe32a8cb267bd9dfe1928b7e6c27ca
asd <asd.it@gmail.com>
2023-09-24 00:31:43 +0330

    init project & create comment component and fetch it

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,91 @@
-import "./App.css";
-
-function App() {
-debugger
-setTimeout(() => {
-  
-  const observer = new IntersectionObserver(entries =>{
-    const boxElm = entries[0];
-    const isIntersecting = boxElm.isIntersecting;
-    boxElm.target.style.backgroundColor = isIntersecting ? "blue" :"red"
-  })
-
-  observer.observe(document.getElementById("box"))
-
-}, 3000);
+import axios from "axios";
+import { useEffect, useState } from "react";
+import pizza from "./assets/pizza.jpg";
+import Comment from "./components/Comment";
+const App = () => {
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const fetchData = async () => {
+    setLoading(true);
+    const data = await (
+      await axios.get("https://react-mini-projects-api.classbon.com/Comments/1")
+    ).data;
+    setComments(data);
+   
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div id="box"
-        style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: "lightgray",
-          margin: "1600px 0",
-        }}
-      ></div>
-    </>
+    <div className="container pt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={pizza} />
+        </div>
+        <div className="col-md-6 pt-4">
+          <div className="product-details pb-3">
+            <div className="mb-3">
+              <span className="h3 fw-normal text-accent">قیمت: 158,000</span>
+            </div>
+            <form className="mb-grid-gutter">
+              <div className="row mx-n2">
+                <div className="col-6 px-2">
+                  <div className="mb-3">
+                    <label className="form-label">اندازه:</label>
+                    <select className="form-select">
+                      <option value="small">کوچک</option>
+                      <option value="medium">متوسط</option>
+                      <option value="large">بزرگ</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="mb-3">
+                    <label className="form-label">تعداد:</label>
+                    <select className="form-select">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <button
+                className="btn btn-info btn-shadow d-block w-100"
+                type="submit"
+              >
+                افزودن به سبد خرید
+              </button>
+            </form>
+            <h5 className="h6 mb-3 pb-3 border-bottom">
+              پیتزا استیک (یک نفره)
+            </h5>
+            <h6 className="fs-sm mb-2">ترکیبات:</h6>
+            <p className="fs-sm">
+              خمیر پیتزا کلاسیک ۲۶ سانتی متری، استیک فیله گوشت، پنیر، قارچ، فلفل
+              دلمه ای، سس کچاپ مخصوص
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-12 pt-5">
+          {loading ? (
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border"></div>
+            </div>
+          ) :comments.map((c)=><Comment key={c.id} {...c}/> )}
+         
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
